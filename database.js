df57144292cc7b308458cec3c53679c2c553ecae
Brendon Bown <bhbochikens@gmail.com>
2019-02-15 15:48:45 -0600

    Refactored database, added `add_user` ability

--- database.js
+++ database.js
@@ -1,26 +1,45 @@
 const fs = require("fs-extra");
 const crypto = require("crypto");
 
+class UserDB {
+    constructor(location) {
+        this.location = location;
+        this.users = fs.readJSONSync(location);
+        this.logged_in = [];
+    }
+
+    has_user(username) {
+        return this.users
+            .filter((user) => user.username === username).length > 0;
+    }
+
+    match(username, password) {
+        return this.users.filter((user) => user.username === username)
+            .filter((user) =>
+                user.password_hash === hash_password(password, user.salt))
+            .length > 0;
+    }
+
+    add_user(username, password, salt) {
+        this.users.push({
+            username: username,
+            password_hash: hash_password(password, salt),
+            salt: salt,
+            verb_practice: [],
+            custom_sets: []
+        });
+    }
+
+    save() {
+        fs.writeFileSync(location);
+    }
+}
+
 function user_db(loc) {
-    let users = fs.readJSONSync(loc);
-    let logged_in = [];
+    const users = fs.readJSONSync(loc);
+    const logged_in = [];
 
-    let db = {
-        has_user: (username) => {
-            return users
-                .filter((user) => user.username === username).length > 0;
-        },
-        match: (username, password) => {
-            return users.filter((user) => user.username === username)
-                .filter((user) => {
-                    let hashed_password = crypto.createHash("md5");
-                    hashed_password.update(user.salt + "" + password);;
-
-                    hashed_password = hashed_password.digest("hex");
-
-                    return user.password === hashed_password;
-                }).length > 0;
-        },
+    {
         // generate_session_id: (username) => {
         //     let id = logged_in
         //         .filter((user) => user.username === username)
@@ -42,7 +61,18 @@ function user_db(loc) {
         // }
     };
 
-    return db;
+    throw "'user_db' is deprecated";
+}
+
+function hash_password(password, salt) {
+    let hashed_password = crypto.createHash("md5");
+    hashed_password.update(salt + "" + password);
+
+    return hashed_password.digest("hex");
+}
+
+function generate_salt() {
+    return crypto.randomBytes(16).toString("hex");
 }
 
 function run_tests() {
@@ -53,6 +83,7 @@ function run_tests() {
     let Test = test.Test;
     let TestSuite = test.TestSuite;
     let assert = test.assert;
+    let assert_eq = test.assert_eq;
 
     let temp_dir = fs.mkdtempSync(path.join(os.tmpdir(), "db-"));
 
@@ -64,7 +95,7 @@ function run_tests() {
     fs.writeFileSync(normal_loc,
         JSON.stringify([{
             username: "user_1",
-            password: hashed_password.read().toString("hex"),
+            password_hash: hashed_password.read().toString("hex"),
             salt: "abc123",
             verb_practice: [],
             custom_sets: []
@@ -76,20 +107,20 @@ function run_tests() {
     .description("Test that each database works")
 
     .add_test(Test.builder()
-        .name("File DB Has User Test")
+        .name("Has User Test")
         
         .description(
             "Test that the database knows " +
             "if it has a user")
         
         .test(() => {
-            let normal = user_db(normal_loc);
+            let normal = new UserDB(normal_loc);
             assert(normal.has_user("user_1"), "Database does not have 'user_1'");
             assert(!normal.has_user("user_2"), "Database has 'user_2'");
         }))
     
     .add_test(Test.builder()
-        .name("File DB Match User to Passwd Test")
+        .name("Match User to Passwd Test")
         
         .description(
             "Test that the database can " +
@@ -97,10 +128,25 @@ function run_tests() {
         )
         
         .test(() => {
-            let normal = user_db(normal_loc);
+            let normal = new UserDB(normal_loc);
             assert(normal.match("user_1", "p@$$w0rd"), "Password did not match username");
             assert(!normal.match("user_1", "password"), "Password matched username");
         }))
+    
+    .add_test(Test.builder()
+        .name("Create User Test")
+        
+        .description(
+            "Ensure that the database can " +
+            "add a user"
+        )
+        
+        .test(() => {
+            let normal = new UserDB(normal_loc);
+            normal.add_user("james_doe", "mynameisjames");
+            assert(normal.match("james_doe", "mynameisjames"));
+        }))
+    
     .build();
     
     TestSuite.run(suite).print_result();
@@ -111,5 +157,7 @@ function run_tests() {
 
 try {
     module.exports.run_tests = exports.run_tests = run_tests;
+    module.UserDB = exports.UserDB = UserDB;
     module.user_db = exports.user_db = user_db;
+    module.generate_salt = exports.generate_salt = generate_salt;
 } catch(error) {}
